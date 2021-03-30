e4a7c62647eee162d40965ac901f4fc7fe7d18cf
choYeongwoo0322 <choyeongwoo.s@gmail.com>
2021-03-30 23:47:59 +0900

    0330

--- django/월말평가 대비/회원가입, 로그인, 로그아웃 연습/articles/urls.py
+++ django/월말평가 대비/회원가입, 로그인, 로그아웃 연습/articles/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name ='articles'
 urlpatterns = [
     path('',views.index, name="index"),
-    path('create',views.create, name="create"),
+    path('create/',views.create, name="create"),
     path('detail/<int:pk>/',views.detail, name="detail"),
     path('update/<int:pk>/',views.update, name="update"),
     path('delete/<int:pk>/',views.delete, name="delete"),
